9c8f2b577ae380dd9d3d3710f7c9a03e62f12b69
aaldulimi <ahmedamedo22@gmail.com>
2022-10-21 22:08:06 +1100

    added support for float encoding/decoding

--- document_database/encoding.py
+++ document_database/encoding.py
@@ -2,27 +2,35 @@
 # to a more faster json library in the future.
 
 import json
+import struct
 
 
 
 # int
-def encode_int(number: int) -> bytes:
-    byte_length = (number.bit_length() + 7) // 8
-    return number.to_bytes(byte_length, 'big')
+def encode_int(x: int) -> bytes:
+    byte_length = (x.bit_length() + 7) // 8
+    return x.to_bytes(byte_length, 'big')
     
 
-def decode_int(number_bytes: bytes) -> int:
-    return int.from_bytes(number_bytes, 'big')
+def decode_int(xbytes: bytes) -> int:
+    return int.from_bytes(xbytes, 'big')
 
+
+# float
+def encode_float(x: float) -> bytes:
+    return struct.pack('<d', x)
+
+def decode_float(xbytes: bytes) -> float:
+    return struct.unpack('<d', xbytes)[0]
 
 
 # str
-def encode_str(text: str) -> bytes:
-    return bytes(text, 'utf-8')
+def encode_str(x: str) -> bytes:
+    return bytes(x, 'utf-8')
  
 
-def decode_str(text_bytes: bytes) -> str:
-    return str(text_bytes.decode())
+def decode_str(xbytes: bytes) -> str:
+    return str(xbytes.decode())
 
 
 
@@ -33,13 +41,15 @@ def encode_array(arr: list) -> bytes:
         return None
 
     arr_str = json.dumps(arr)
-    arr_bytes = arr_str.encode("utf-8")
+    arr_bytes = arr_str.encode('utf-8')
 
     return arr_bytes
 
 
 def decode_array(arr: bytes) -> list:
-    arr_decoded = arr.decode("utf-8")
+    arr_decoded = arr.decode('utf-8')
     arr = json.loads(arr_decoded)
     
     return arr
+
+
