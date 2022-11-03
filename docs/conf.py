0937339e95cdf331f6004e6e620a026dd3364151
Ahmed <ahmedamedo22@gmail.com>
2022-11-04 01:34:46 +1100

    added furo to conf extensions

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ release = '0.2.9'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'furo']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
