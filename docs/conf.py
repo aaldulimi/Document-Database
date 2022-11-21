95e7e7150f2c3ed00455d6add0deaa921a1492d3
Ahmed <ahmedamedo22@gmail.com>
2022-11-21 10:35:42 +0800

    fixing sphinx autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -1,11 +1,11 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('./../..'))
 
 project = 'RockyDB'
 copyright = '2022, Ahmed'
 author = 'Ahmed'
-release = '0.2.9'
+release = '0.2.10'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
